6d47bba8b9951a54a831cfaea030b284e51995a8
Milo Hartsoe <milo.red.duck@gmail.com>
2016-10-07 00:12:38 +0000

    add index.html

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var timezoneDiff = 14400;
-var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
+var months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 app.get("/:date", function(req, res) {
     var raw = req.params.date;
-    var date;
-    if(isNaN(raw)){//given string
-         date = new Date(Date.parse(decodeURI(raw)));
+    var date, json;
+    if (isNaN(raw)) { //given string
+        date = new Date(Date.parse(decodeURI(raw)));
     }
-    else{//given unix
+    else { //given unix
         date = new Date(+raw * 1000);
     }
-    var ret = '{ "unix": ' + (date.getTime()/1000 - timezoneDiff)
-                + ', "natural": "'+ months[date.getMonth()] + ' ' 
-                + date.getDate() +', ' + date.getFullYear()+'" }';
-    res.end(ret);
+    if( isNaN(date.getTime())){
+        json ={unix: null, natural: null};
+    }
+    else{
+    json = { unix: (date.getTime() / 1000 - timezoneDiff),
+        natural: (months[date.getMonth()] + ' ' +
+        date.getDate() + ', ' + date.getFullYear()) };
+    }
+    console.log(json);
+    res.json(json);
 });
 
-app.listen(8080, function () {
-  console.log('Timestamp app listening on port 8080');
+app.get('/', function(req, res) {
+    var file = path.join(__dirname, 'index.html');
+    res.sendFile(file, function(err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+        else {
+            console.log('Sent:', file);
+        }
+    });
+});
+
+app.listen(8080, function() {
+    console.log('Timestamp app listening on port 8080');
 });
